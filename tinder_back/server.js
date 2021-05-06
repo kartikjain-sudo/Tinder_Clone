70fe95ccaee24b25a1e9088d5af4aceadaf9c009
kartik jain <kartikjain42@protonmail.com>
2021-05-06 21:59:36 +0530

    need debugging

--- tinder_back/server.js
+++ tinder_back/server.js
@@ -1,12 +1,58 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Cors = require('cors');
+const {username, password} = require('./secret.json');
+// const { default: dbCards } = require('./dbCards');
+// const Cards = require('dbCards.js')
 
 const app = express();
 const port = process.env.PORT || 8001;
+const connection_url = `mongodb+srv://${username}:${password}@cluster0.ziepk.mongodb.net/tinderdb?retryWrites=true&w=majority`;
 
-app.get('/', (req,res) => {
+// console.log({connection_url});
+
+app.use(express.json());
+app.use(Cors());
+
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+
+const cardSchema = mongoose.Schema({
+    name: String,
+    imgUrl: String
+})
+
+const Cards = mongoose.model('Cards', cardSchema);
+
+app.get('/', (req, res) => {
     res.status(200).send("Hello, World!");
 })
+
+app.post('/tinder/cards', (req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    })
+});
+
+app.get('/tinder/cards', (req, res) => {
+    // console.log("ABC");
+    Cards.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    });
+});
 
 app.listen(port, () => {
     console.log(`listening on ${port}`);
--- tinder_front/src/TinderCards.js
+++ tinder_front/src/TinderCards.js
@@ -1,19 +1,26 @@
 //rfce
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
 
+const axios = require('axios');
+
+const instance = axios.create({
+    baseURL: 'http://localhost:8001'
+})
+
 function TinderCards() {
-    const [people, setPeople] = useState([
-        {
-            name: 'Elon Musk',
-            url: "https://image.cnbcfm.com/api/v1/image/105312310-1533762032624elon.jpg?v=1552483743"
-        },
-        {
-            name: 'Kakashi Hatake',
-            url: "https://images.hdqwalls.com/download/naruto-anime-on-1680x1050.jpg"
+    const [people, setPeople] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const req = axios.get('/tinder/cards');
+            setPeople(req.data);
         }
-    ]);
+        fetchData();
+    }, [])
+
+    console.log({people});
 
     const swiped = (direction, nameToDelete) => {
         console.log("removing: " + nameToDelete);
@@ -35,7 +42,7 @@ function TinderCards() {
                         onSwipe={(dir) => swiped(dir, person.name)}
                         onCardLeftScreen = {() => outOfFrame(person.name)}>
                             
-                            <div className="card" style = {{backgroundImage: `url(${person.url})`}}>
+                            <div className="card" style = {{backgroundImage: `url(${person.imgUrl})`}}>
                                 <h3>{person.name}</h3>
                             </div>
                     </TinderCard>
